4f12645edc2f24bd4b6522a8a2e5e929faccaadc
Luca Adam <luca.adam23@gmail.com>
2023-11-06 12:53:03 +0100

    Ajout du table

--- src/app/Pages/client/list-client/list-client.component.ts
+++ src/app/Pages/client/list-client/list-client.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-client',
   templateUrl: './list-client.component.html',
   styleUrls: ['./list-client.component.css']
 })
-export class ListClientComponent {
+export class ListClientComponent implements OnInit {
+  dtOptions: any = {};
+
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers'
+    };
+  }
 
 }
